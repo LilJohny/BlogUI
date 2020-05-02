7546a28509f26e729b4a4813c46f016468d7a51e
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-02 23:49:23 +0300

    More of material-ui deps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './css/App.css';
+import 'typeface-roboto';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </head>
+      <div className="App">
+
+      </div>
+    </html>
   );
 }
 
