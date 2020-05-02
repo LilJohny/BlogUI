85e83ee4024d9566463c1818c86630c489732f55
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-02 23:35:57 +0300

    Implemented ArticleImage component

---
+++ src/ArticleImage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/ArticleImage.css';
+
+
+export default class ArticleImage extends React.Component {
+    constructor(source, alternativeText) {
+        super();
+        this.src = source;
+        this.alt = alternativeText;
+    }
+    render() {
+        return (<img src={this.src} alt={this.alt}></img>)
+    }
+}
