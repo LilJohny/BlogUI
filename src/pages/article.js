0423ae2f1119f90c3151f4dada934c9cffbafd28
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-03 20:31:05 +0300

    Created ArticlePage component

---
+++ src/pages/article.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ArtileNavBar from '../components/ArticleNavBar';
+
+
+export default class ArticlePage extends React.Component {
+    render() {
+        return (ArtileNavBar);
+    }
+}
