25f5a245622be5e38c20cfd482980137be1ab79f
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-03 18:55:30 +0300

    Implemented avatar component

---
+++ src/components/AvatarImage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange, green } from '@material-ui/core/colors';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    large: {
+        width: theme.spacing(50),
+        height: theme.spacing(50),
+    },
+    rounded: {
+        color: '#fff',
+        backgroundColor: green[500],
+    },
+}));
+
+export default function VariantAvatars(source, alt = '') {
+    const classes = useStyles();
+    const classNames = `${classes.rounded} ${classes.large}`
+    return (
+        <div className={classes.root}>
+            <Avatar variant="rounded" className={classNames}>
+                <img src={source} alt={alt}></img>
+            </Avatar>
+        </div>
+    );
+}
