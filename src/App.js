3d019fd78516cd3e58ed148ca098bc03e92a9e6c
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-03 19:03:36 +0300

    Returned needed app dependencies

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './css/App.css';
 import 'typeface-roboto';
 import React from 'react';
-import NavBar from './components/NavBar';
-import BackToTop from './components/ArticleNavBar'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; // add
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import VariantAvatars from './components/AvatarImage';
 
 
 function App() {
   return (
-    <BackToTop/>
+    <html>
+      <head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </head>
+      <div className="App">
+
+      </div>
+    </html>
   );
 }
 
