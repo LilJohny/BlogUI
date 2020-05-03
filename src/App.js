6575d318e28860a1b6885a6e838be4f22eeaa617
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-03 21:13:03 +0300

    Fixed app js

--- src/App.js
+++ src/App.js
@@ -2,36 +2,25 @@ import logo from './logo.svg';
 import 'typeface-roboto';
 import './css/App.css'
 import React from 'react';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import ProfilePage from './pages/profile';
+import ArticlePage from "./pages/article";
+import {MuiThemeProvider} from "material-ui";
+
 
 function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-        },
-      }),
-    [prefersDarkMode],
-  );
-  return (
-    <React.Fragment>
-
-      <html>
-        <head>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        </head>
-        <div className="App">
-        <ProfilePage/>
-        </div>
-      </html>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <MuiThemeProvider>
+                <html>
+                <head>
+                    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+                </head>
+                <div className="App">
+                    <ArticlePage/>
+                </div>
+                </html>
+            </MuiThemeProvider>
+        </React.Fragment>
+    );
 }
 
 export default App;
