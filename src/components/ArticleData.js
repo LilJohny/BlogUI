6d6cd7bee8a681f58c8ce07bedd57ea159452d80
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-04 03:47:45 +0300

    Fixed Author name

--- src/components/ArticleData.js
+++ src/components/ArticleData.js
@@ -6,10 +6,11 @@ export default class ArticleData extends React.Component {
     render() {
         return (
             <div className="ArticleData">
-                <div>
-                <AvatarImage className={"pic"} src={this.props.imageSrc} alt={this.props.imageAlt} variant={"circle"}/>
-                <p>{this.props.authorName}</p>
-                <time dateTime={this.props.dateTime}/>
+                <div className={"flexbox"}>
+                    <span><AvatarImage className={"pic"} src={this.props.imageSrc} alt={this.props.imageAlt}
+                                       variant={"circle"}/></span>
+                    <span><p>{this.props.authorName}</p></span>
+                    <span><time dateTime={this.props.dateTime}/></span>
                 </div>
             </div>
         );
