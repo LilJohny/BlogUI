a08eb70ae9e4b4e2a5a72c973134b8646b82423e
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-04 05:41:33 +0300

    First version of side bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,15 @@ import FaceIcon from '@material-ui/icons/Face';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from 'material-ui/IconButton';
 import '../css/NavBar.css';
+import MenuItem from "@material-ui/core/MenuItem";
+import Drawer from "@material-ui/core/Drawer";
 
 export default class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
     render() {
         let toolbar;
         if (this.props.article === true) {
@@ -13,16 +20,21 @@ export default class NavBar extends React.Component {
         } else {
             toolbar = <Toolbar/>;
         }
+        this.handleToggle = () => this.setState({open: !this.state.open})
         return (<React.Fragment>
             <AppBar position="sticky">
                 <Toolbar>
-                    <IconButton className={"menu"} aria-label="Menu" color="white">
+                    <IconButton className={"menu"} aria-label="Menu" color="white" onClick={this.handleToggle}>
                         <MenuIcon/>
                     </IconButton>
+                    <Drawer open={this.state.open}>
+                        <MenuItem>Menu Item</MenuItem>
+                        <MenuItem>Menu Item 2</MenuItem>
+                    </Drawer>
                     <section className={"rightToolBar"}>
-                    <IconButton className={"profile"} aria-label="My profile" color="white">
-                        <FaceIcon/>
-                    </IconButton>
+                        <IconButton className={"profile"} aria-label="My profile" color="white">
+                            <FaceIcon/>
+                        </IconButton>
                     </section>
                 </Toolbar>
             </AppBar>
