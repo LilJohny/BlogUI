11385e0bf30d857b03ef2b102deda0adf281e3c9
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-06 13:39:31 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './css/App.css'
 import React from 'react';
 import ArticlePage from "./pages/article";
 import ProfilePage from "./pages/profile";
-import {MuiThemeProvider} from "material-ui";
+import { MuiThemeProvider } from "material-ui";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 
@@ -12,26 +12,23 @@ function App() {
     return (
         <MuiThemeProvider>
             <div className="App">
-                <CssBaseline/>
-                {/*                       <ArticlePage
+                <CssBaseline />
+                <ArticlePage
                     imageSrc={"https://frontendmasters.com/static-assets/learn/og-learning-path-react.jpg"}
                     text={[...new Array(100)]
                         .map(
-                           () => `Cras mattis consectetur purus sit amet fermentum.
+                            () => `Cras mattis consectetur purus sit amet fermentum.
                                    Cras justo odio, dapibus ac facilisis in, egestas eget quam.
                                    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-                       )
-                       .join('\n')}
-                   authorImageSrc={"https://lh5.googleusercontent.com/--OlyHl449xI/AAAAAAAAAAI/AAAAAAAAAAA/ACevoQNk7ZZElQ_vKIQT_6d4HZw_wN3Qxg/mo/photo.jpg?sz=64"}
-                   authorName={"Denis Ivanenko"}
-                   dateTime={"2017-02-14"}
-                   title={"Test Article Title"}
-                   subtitle={"This is test subtitle"}
-               />*/}
-                {/* The above commented code will display Article Page with dummy data./>*/}
-                <ProfilePage profileName={"Denis Ivanenko"} dateTime={"2017-02-14"}
-                             userBio={"Example of User Bio. Here User can write about himself."}/>
+                        )
+                        .join('\n')}
+                    authorImageSrc={"https://lh5.googleusercontent.com/--OlyHl449xI/AAAAAAAAAAI/AAAAAAAAAAA/ACevoQNk7ZZElQ_vKIQT_6d4HZw_wN3Qxg/mo/photo.jpg?sz=64"}
+                    authorName={"Denis Ivanenko"}
+                    dateTime={"2017-02-14"}
+                    title={"Test Article Title"}
+                    subtitle={"This is test subtitle"}
+                />
             </div>
         </MuiThemeProvider>
     );
--- src/components/ArticleData.js
+++ src/components/ArticleData.js
@@ -6,13 +6,11 @@ export default class ArticleData extends React.Component {
     render() {
         return (
             <div className="ArticleData">
-                <div className={"flexbox"}>
-                    <span><AvatarImage className={"pic"} src={this.props.imageSrc} alt={this.props.imageAlt}
-                        variant={"circle"} /></span>
-                    <span><p>{this.props.authorName}</p>
-                        <p>Posted on <time dateTime={this.props.dateTime}>{this.props.dateTime}</time> </p>
-                    </span>
-                </div>
+                <span><AvatarImage className={"pic"} src={this.props.imageSrc} alt={this.props.imageAlt}
+                    variant={"circle"} /></span>
+                <span><p>{this.props.authorName}</p>
+                    <p>Posted on <time dateTime={this.props.dateTime}>{this.props.dateTime}</time> </p>
+                </span>
             </div>
         );
     }
