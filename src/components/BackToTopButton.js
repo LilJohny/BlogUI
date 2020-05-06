7f7cd2a2af3b226d1f4122f95915f9d0b51a4b36
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-06 13:42:07 +0300

    More Refactoring

--- src/components/BackToTopButton.js
+++ src/components/BackToTopButton.js
@@ -22,9 +22,7 @@ function ScrollTop(props) {
     });
 
     const handleClick = event => {
-        const anchor = (event.target.ownerDocument || document).querySelector(
-            "#back-to-top-anchor"
-        );
+        const anchor = (event.target.ownerDocument || document).getElementById("back-to-top-anchor");
 
         if (anchor) {
             anchor.scrollIntoView({ behavior: "smooth", block: "center" });
