5eee275667bb1e9a062ffd954dcea4867985ad9b
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-10 01:46:34 +0300

    Implemented TabPanels

--- src/components/TabsMenu.js
+++ src/components/TabsMenu.js
@@ -3,13 +3,47 @@ import {makeStyles} from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import * as PropTypes from "prop-types";
 
+function TabPanel(props) {
+    const {children, value, index, ...other} = props;
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`sinple-tab-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
 
 const useStyles = makeStyles({
     root: {
-        flexGrow: 1
+        flexGrow: 1,
     }
 });
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 export default function CenteredTabs() {
     const classes = useStyles();
@@ -26,10 +60,19 @@ export default function CenteredTabs() {
                 textColor="primary"
                 centered
             >
-                <Tab label="Item One"/>
-                <Tab label="Item Two"/>
-                <Tab label="Item Three"/>
+                <Tab label="Item One" {...a11yProps(0)}/>
+                <Tab label="Item Two" {...a11yProps(1)}/>
+                <Tab label="Item Three" {...a11yProps(2)}/>
             </Tabs>
+            <TabPanel value={value} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
         </Paper>
     );
 }
