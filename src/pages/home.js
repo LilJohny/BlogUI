0e304f5cb2326b1691253153d0aaa20a118f558e
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-10 00:32:56 +0300

    Formatting

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,10 @@
 import React from "react";
 import CenteredTabs from "../components/TabsMenu";
-export default class HomePage extends React.Component{
+
+export default class HomePage extends React.Component {
     render() {
         return (
-         <CenteredTabs/>
+            <CenteredTabs/>
         );
     }
 }
