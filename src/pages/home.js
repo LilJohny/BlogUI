8f76ece362547141b5979ceea3f680ed2955eadc
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-10 00:32:17 +0300

    Implemented Centered tabs component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import CenteredTabs from "../components/TabsMenu";
 export default class HomePage extends React.Component{
     render() {
         return (
-         <h1>Home</h1>
+         <CenteredTabs/>
         );
     }
 }
