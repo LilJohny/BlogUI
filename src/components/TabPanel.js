d7f4e5c2fe7c52b708355714dd46502902edc0e7
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-16 18:45:00 +0300

    Fix

--- src/components/TabPanel.js
+++ src/components/TabPanel.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import * as PropTypes from "prop-types";
 
 export default function TabPanel(props) {
     const {children, value, index, ...other} = props;
