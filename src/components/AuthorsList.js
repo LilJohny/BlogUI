e07e8114f7a7ada47a469343142378882ed06557
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-17 08:21:49 +0300

    Refactoring

--- src/components/AuthorsList.js
+++ src/components/AuthorsList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import List from "@material-ui/core/List";
-import {ListItem} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
+import { ListItem } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import AuthorCard from "./AuthorCard";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,13 +18,7 @@ export default function AuthorsList(props) {
         <List component="nav" aria-label="secondary mailbox folders">
             {authors.map((author) =>
                 <ListItem>
-                    <AuthorCard name={author.name}
-                                bio={author.bio}
-                                authorImageSrc={author.authorImageSrc}
-                                link={author.link}
-                                reputation={author.reputation}
-                                imageAlt={author.imageAlt}
-                    />
+                    <AuthorCard {...author} />
                 </ListItem>)}
         </List>
     </div>);
