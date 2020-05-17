5ff704a5b5dcaee5f9bca02a819f2c8e8face36d
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-17 12:35:35 +0300

    Refactoring

--- src/components/TabPanel.js
+++ src/components/TabPanel.js
@@ -5,7 +5,7 @@ import * as PropTypes from "prop-types";
 
 export default function TabPanel(props) {
     const { children, value, index, ...other } = props;
-    return (
+    return value === index && (
         <div
             role="tabpanel"
             hidden={value !== index}
@@ -13,11 +13,9 @@ export default function TabPanel(props) {
             aria-labelledby={`simple-tab-${index}`}
             {...other}
         >
-            {value === index && (
-                <Box p={3}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
+            <Box p={3}>
+                <Typography>{children}</Typography>
+            </Box>
         </div>
     );
 }
