6725ae51c5a503fda8f646776849e01ae06ae2e2
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-18 07:44:29 +0300

    Removed obsolete imports

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 import SimpleList from "../components/SimpleList";
 import NavBar from "../components/NavBar";
-import { render } from "@testing-library/react";
 import axios from "axios";
 import { withStyles } from "@material-ui/styles";
 
--- src/pages/interesting.js
+++ src/pages/interesting.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ArticlesList from "../components/ArticlesList";
 import { Paper } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import NavBar from "../components/NavBar";
 import { withStyles } from "@material-ui/styles";
 import axios from "axios";
