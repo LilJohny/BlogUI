42a0acb55ff71818da15a1f1516ffd7c8e7c8155
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-30 02:07:40 +0300

    fixed rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,13 +1,16 @@
 import { DATA_LOADED, DRAWER_TOGGLED } from '../actions/types';
-export default function rootReducer(state, action) {
+import { initialState } from '../store';
+
+export default function rootReducer(state=initialState, action) {
     switch (action.type) {
         case DATA_LOADED: {
-             return {content: action.content, dataLoaded:true, drawerToggled:state.drawerToggled}
+            return { content: action.content, dataLoaded: true, drawerToggled: state.drawerToggled };
         }
         case DRAWER_TOGGLED: {
-            return { content:state.content, dataLoaded:state.dataLoaded, drawerToggled:true }
+            return { content: state.content, dataLoaded: state.dataLoaded, drawerToggled: true };
         }
         default:
             return state;
     }
+
 }
