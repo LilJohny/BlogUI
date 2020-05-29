fd701bb8b955db2c7ac7613bac784d311e8cf8ff
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-29 23:09:47 +0300

    New field to state added

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, compose, applyMiddleware } from "redux";
 import rootReducer from './reducers/root';
 import ReduxThunk from 'redux-thunk';
 
-const initialState = { content: [], dataLoaded:false };
+const initialState = { content: [], dataLoaded:false, drawerToogled:false };
 
 export default function configStore() {
     return createStore(
