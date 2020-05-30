9b4c1f179fd9381a9b797dd467b5ec91359fdf6d
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-30 16:40:05 +0300

    NavBar works!

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -26,70 +26,71 @@ const useStyles = makeStyles(theme => ({
         background: "#485461"
     }
 }));
-function NavBar(props){
-        const toolbar = React.createRef();
-        let toolbarElement = props.article === true ? <Toolbar id="back-to-top-anchor" ref={toolbar} /> : <Toolbar />;
-        let button = null;
-        if (props.article === true) {
-            button = <BackToTopButton anchor={toolbar} />;
-        }
-        const classes  = useStyles;
-        const navbar_links = [["Home", "/"], ["Categories", "/categories"], ["Interesting Posts For You", "/interesting"], ["Your Inbox", "/inbox"], ["Register account","/registration"]];
-        const sideList = (
-            <div className={classes.list} role="presentation" onClick={props.toggleDrawer(false)}
-                onKeyDown={props.toggleDrawer(false)}>
-                <List>
-                    {navbar_links.map((text) => (
-                        <ListItem component="a" button key={text[0]} href={text[1]}>
-                            <ListItemText>{text[0]}</ListItemText>
-                        </ListItem>
-                    ))}
-                </List>
-            </div>
-        );
-    const toggleDrawer = (open) => { props.toggleDrawer(open) };
-    const drawerToggled = props.drawerToggled;
-        return (
-            <React.Fragment>
-                <Router>
-                    <AppBar position="sticky">
-                        <Toolbar>
-                            <IconButton className={"menu"} aria-label="Menu" color="white"
-                                onClick={toggleDrawer(true)}>
-                                <MenuIcon />
-                            </IconButton>
-                            <Drawer
-                                classes={{ paper: classes.paper }}
-                                open={drawerToggled}
-                                onClose={toggleDrawer(false)}
-                            >
-                                <div
-                                    tabIndex={0}
-                                    role="button"
-                                    onClick={toggleDrawer(false)}
-                                    onKeyDown={toggleDrawer(false)}
-                                    className={{ root: classes.root }}
-                                >
-                                    {sideList}
-                                </div>
-                            </Drawer>
-
-                            <section className={"rightToolBar"}>
-                                <IconButton className={"profile"} aria-label="My profile" color={"black"} href="/profile">
-                                    <FaceIcon />
-                                </IconButton>
-                            </section>
-                        </Toolbar>
-                    </AppBar>
-                    {toolbarElement}
-                </Router>
-                {button}
-            </React.Fragment>
-        );
+function NavBar(props) {
+    const toolbar = React.createRef();
+    let toolbarElement = props.article === true ? <Toolbar id="back-to-top-anchor" ref={toolbar} /> : <Toolbar />;
+    let button = null;
+    if (props.article === true) {
+        button = <BackToTopButton anchor={toolbar} />;
     }
+    const toggleDrawer = (open) => { props.toggleDrawer(open) };
+    const classes = useStyles;
+    const navbar_links = [["Home", "/"], ["Categories", "/categories"], ["Interesting Posts For You", "/interesting"], ["Your Inbox", "/inbox"], ["Register account", "/registration"]];
+    const sideList = (
+        <div className={classes.list} role="presentation" onClick={()=>toggleDrawer(false)}
+            onKeyDown={()=>toggleDrawer(false)}>
+            <List>
+                {navbar_links.map((text) => (
+                    <ListItem component="a" button key={text[0]} href={text[1]}>
+                        <ListItemText>{text[0]}</ListItemText>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    const drawerToggled = props.drawerToggled;
+    return (
+        <React.Fragment>
+            <Router>
+                <AppBar position="sticky">
+                    <Toolbar>
+                        <IconButton className={"menu"} aria-label="Menu" color="white"
+                            onClick={()=>toggleDrawer(true)}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Drawer
+                            classes={{ paper: classes.paper }}
+                            open={drawerToggled}
+                            onClose={()=>toggleDrawer(false)}
+                        >
+                            <div
+                                tabIndex={0}
+                                role="button"
+                                onClick={()=>toggleDrawer(false)}
+                                onKeyDown={()=>toggleDrawer(false)}
+                                className={{ root: classes.root }}
+                            >
+                                {sideList}
+                            </div>
+                        </Drawer>
+
+                        <section className={"rightToolBar"}>
+                            <IconButton className={"profile"} aria-label="My profile" color={"black"} href="/profile">
+                                <FaceIcon />
+                            </IconButton>
+                        </section>
+                    </Toolbar>
+                </AppBar>
+                {toolbarElement}
+            </Router>
+            {button}
+        </React.Fragment>
+    );
+}
 
 const mapStateToProps = (state) => ({
-    drawerToggled:state.drawerToggled
+    drawerToggled: state.drawerToggled
 });
 const mapDispatchToProps = (dispatch) => ({
     toggleDrawer: (open) => dispatch(toggleDrawer(open)),
