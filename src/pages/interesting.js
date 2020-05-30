6e96a6f02af4899265beee2e05004bc37bf88e27
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-30 04:59:29 +0300

    Reimplemented with redux

--- src/pages/interesting.js
+++ src/pages/interesting.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ArticlesList from "../components/ArticlesList/ArticlesList";
 import { Paper } from "@material-ui/core";
 import NavBar from "../components/NavBar/NavBar";
-import { withStyles } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/core/styles";
+import { connect } from 'react-redux';
+import { loadData } from '../actions';
 
-
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
         maxWidth: 550,
@@ -18,42 +19,37 @@ const styles = theme => ({
         marginRight: "auto",
         marginLeft: "auto"
     }
-});
+}));
 
-class InterestingPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: [],
-            dataLoaded: false
-        }
+function InterestingPage(props) {
+    useEffect(() => {
+        const links = ["http://localhost:3001/articles", "http://localhost:3001/authors"]
+        props.loadData(links);
+    }, []);
+    const articlesLoaded = props.articles !== undefined && props.articles.length !== 0;
+    const classes = useStyles();
+    if (!articlesLoaded) {
+        return <p>Loading data</p>
+    } else {
+        const articles = props.articles;
+        return (
+            <React.Fragment>
+                <NavBar />
+                <Paper className={classes.root}>
+                    <h1 className={classes.centered}>Interesting Posts For You: </h1>
+                    <ArticlesList className={classes.centered} articles={articles} />
+                </Paper>
+            </React.Fragment >);
     }
-    componentDidMount() {
-        this.getData();
-    }
-    async getData() {
-        //let articles = await axios.get("http://localhost:3001/articles");
-        const articles = [];
-        this.setState({ articles: articles, dataLoaded: true });
-    }
-    render() {
-        if (!this.state.dataLoaded) {
-            return <p>Loading data</p>
-        } else {
-            const { classes } = this.props;
-            const articles = this.state.articles.data;
-            return (
-                <React.Fragment>
-                    <NavBar />
-                    <Paper className={classes.root}>
-                        <h1 className={classes.centered}>Interesting Posts For You: </h1>
-                        <ArticlesList className={classes.centered} articles={articles} />
-                    </Paper>
-                </React.Fragment >);
-        }
 
-    }
 }
 
+const mapStateToProps = (state) => ({
+    articles: state.articles,
+    dataLoaded: state.dataLoaded
+});
+const mapDispatchToProps = (dispatch) => ({
+    loadData: (link) => dispatch(loadData(link)),
+});
 
-export default withStyles(styles)(InterestingPage);
+export default connect(mapStateToProps, mapDispatchToProps)(InterestingPage);
