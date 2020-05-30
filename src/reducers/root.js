8254cab395d7491459ae10ec5a63897ab04ee5bd
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-30 04:51:02 +0300

    Added new action to reduedr

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,13 +1,21 @@
-import { DATA_LOADED, DRAWER_TOGGLED } from '../actions/types';
+import { NEW_DATA_LOADED, DATA_LOADED, DRAWER_TOGGLED } from '../actions/types';
 import { initialState } from '../store';
 
-export default function rootReducer(state=initialState, action) {
+export default function rootReducer(state = initialState, action) {
     switch (action.type) {
         case DATA_LOADED: {
-            return { content: action.content, dataLoaded: true, drawerToggled: state.drawerToggled };
+            return { categories: state.categories, authors: state.authors, articles: state.articles, drawerToggled: state.drawerToggled };
         }
         case DRAWER_TOGGLED: {
-            return { content: state.content, dataLoaded: state.dataLoaded, drawerToggled: true };
+            return { categories: state.categories, authors: state.authors, articles: state.articles, drawerToggled: true };
+        }
+        case NEW_DATA_LOADED: {
+            const keyValue = action.keyValue;
+            const content = action.content;
+            console.log(action);
+            let newState = { categories: state.categories, authors: state.authors, articles: state.articles, drawerToggled: state.drawerToggled };;
+            newState[keyValue] = content;
+            return newState;
         }
         default:
             return state;
